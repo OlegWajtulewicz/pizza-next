0f3e8ccf87079de1171dae6b625c469329050b3b
aleh vaitulevich <vaitulevichaleh@gmail.com>
2024-09-21 16:02:42 +0200

    proba1

--- app/(dashboard)/dashboard/product-items/page.tsx
+++ app/(dashboard)/dashboard/product-items/page.tsx
@@ -73,7 +73,7 @@ export default function DashboardProductItems() {
           price: '',
           productId: '',
           size: '',
-          pizzaType: ''
+          pizzaType: '',
         }} 
         onSave={handleSave}
         />
